d8e3ae342bb2440cbdada991550f51c790b7c973
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2023-01-21 10:05:46 +0500

    sideNav fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import { Route, Routes } from "react-router-dom";
+// import { ProSidebarProvider } from "react-pro-sidebar/dist/components/ProSidebarProvider";
 import Topbar from "./scenes/global/Topbar";
-import Dashboard from "./scenes/dashboard/Dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
   const [color, colorMode] = useMode();
@@ -10,14 +22,27 @@ function App() {
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={color}>
         <CssBaseline />
+        {/* <ProSidebarProvider> */}
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />}></Route>
+              {/* <Route path="/team" element={<Team />}></Route> */}
+              {/* <Route path="/contacts" element={<Contacts />}></Route> */}
+              {/* <Route path="/invoices" element={<Invoices />}></Route> */}
+              {/* <Route path="/form" element={<Form />}></Route> */}
+              {/* <Route path="/bar" element={<Bar />}></Route> */}
+              {/* <Route path="/pie" element={<Pie />}></Route> */}
+              {/* <Route path="/line" element={<Line />}></Route> */}
+              {/* <Route path="/faq" element={<FAQ />}></Route> */}
+              {/* <Route path="/geography" element={<Geography />}></Route> */}
+              {/* <Route path="/calendar" element={<Calendar />}></Route> */}
             </Routes>
           </main>
         </div>
+        {/* </ProSidebarProvider> */}
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
