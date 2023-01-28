d5c7d0e480f24db25890bf1599f6e5d602db855b
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2023-01-28 10:52:22 +0500

    Contacts added

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
 // import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -28,7 +28,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />}></Route>
               <Route path="/team" element={<Team />}></Route>
-              {/* <Route path="/contacts" element={<Contacts />}></Route> */}
+              <Route path="/contacts" element={<Contacts />}></Route>
               {/* <Route path="/invoices" element={<Invoices />}></Route> */}
               {/* <Route path="/form" element={<Form />}></Route> */}
               {/* <Route path="/bar" element={<Bar />}></Route> */}
@@ -41,7 +41,6 @@ function App() {
           </main>
         </div>
       </ThemeProvider>
-      //{" "}
     </ColorModeContext.Provider>
   );
 }
