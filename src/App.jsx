41654c4de929ffcd5b1aef937581a41edc870265
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2023-01-29 10:52:42 +0500

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
-// import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
+import Calendar from "./scenes/calendar";
+import FAQ from "./scenes/faq";
+import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import Geography from "./scenes/geography";
 
 function App() {
   const [color, colorMode] = useMode();
@@ -31,12 +31,12 @@ function App() {
               <Route path="/contacts" element={<Contacts />}></Route>
               <Route path="/invoices" element={<Invoices />}></Route>
               <Route path="/form" element={<Form />}></Route>
-              {/* <Route path="/bar" element={<Bar />}></Route> */}
-              {/* <Route path="/pie" element={<Pie />}></Route> */}
-              {/* <Route path="/line" element={<Line />}></Route> */}
-              {/* <Route path="/faq" element={<FAQ />}></Route> */}
-              {/* <Route path="/geography" element={<Geography />}></Route> */}
-              {/* <Route path="/calendar" element={<Calendar />}></Route> */}
+              <Route path="/calendar" element={<Calendar />}></Route>
+              <Route path="/faq" element={<FAQ />}></Route>
+              <Route path="/bar" element={<Bar />}></Route>
+              <Route path="/pie" element={<Pie />}></Route>
+              <Route path="/line" element={<Line />}></Route>
+              <Route path="/geography" element={<Geography />}></Route>
             </Routes>
           </main>
         </div>
